a33ab6aaaa6f7ac6294d121082e8eb4178d84226
BenB <boyer.benlee@gmail.com>
2025-10-29 13:13:01 -0700

    add story content

--- src/story/story.js
+++ src/story/story.js
@@ -1,3 +1,10 @@
 export default function Story() {
-  return <p>Story works!</p>;
+  return (
+    <p>
+      Yojik is Maya's baby pet hedgehog. He was first introduced in the Maya's Adventures series and due to his
+      popularity commanded his own book series. Yojik has a lot of fun most every day. Sometimes Yojik gets into trouble
+      but usually he successfully navigates the twists and turns that enter into his life. When Yojik is unable to save
+      himself, Maya is always there to rescue him. These are stories about Yojik's life from the beginning until now.
+    </p>
+  );
 }
