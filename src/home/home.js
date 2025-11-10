2a81f3b5abed171f2662a50eeee42f8d9acab9e5
BenB <boyer.benlee@gmail.com>
2025-11-10 11:39:53 -0800

    align titel text on homepage

--- src/home/home.js
+++ src/home/home.js
@@ -2,7 +2,7 @@ import Story from '../story/story';
 export default function Home() {
   return (
     <div className='col justify-c align-c view-width vw-75'>
-      <p className='title-font font-faded-outline white mb-l'>Yojik the Hedgehog Books</p>
+      <p className='title-font font-faded-outline white mb-m txt-align-c'>Yojik the Hedgehog Books</p>
       <Story />
     </div>
   );
