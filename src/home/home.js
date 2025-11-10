5ecb944578ce208feedd6610e5d207fc9e602146
BenB <boyer.benlee@gmail.com>
2025-11-10 11:31:16 -0800

    home page modeling

--- src/home/home.js
+++ src/home/home.js
@@ -1,8 +1,8 @@
 import Story from '../story/story';
 export default function Home() {
   return (
-    <div className='col justify-c align-c'>
-      <p className='title-font font-faded-outline white'>Yojik the Hedgehog Books</p>
+    <div className='col justify-c align-c view-width vw-75'>
+      <p className='title-font font-faded-outline white mb-l'>Yojik the Hedgehog Books</p>
       <Story />
     </div>
   );
