d6dfe0605103432bcc21f703ca16bb84b3ed9835
bboy <boyer.benlee@gmail.com>
2026-05-23 19:32:04 -0700

    add icon, translation notes

--- src/books/books.js
+++ src/books/books.js
@@ -65,12 +65,21 @@ export default function Books() {
           <text className='book-viewer'>&#10148;</text>
         </button>
       </div>
-      <div className='books col txt-m mb-s'>
+      <div className='books col txt-m mb-xs'>
         <a
           href={bookEntries[bookIndex][1]}
           className='p-xs button-glow soft-edges bg-white solid-border border-2 border-black'>
           See on Amazon
         </a>
+      </div>
+      <div className="books row justify-c align-c w-100 my-xs px-xs red-2 bg-white rounded txt-reg">
+        <span className='mt-xs mr-xs'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-alert-square-rounded">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 2l.642 .005l.616 .017l.299 .013l.579 .034l.553 .046c4.687 .455 6.65 2.333 7.166 6.906l.03 .29l.046 .553l.041 .727l.006 .15l.017 .617l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.455 4.687 -2.333 6.65 -6.906 7.166l-.29 .03l-.553 .046l-.727 .041l-.15 .006l-.617 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.687 -.455 -6.65 -2.333 -7.166 -6.906l-.03 -.29l-.046 -.553l-.041 -.727l-.006 -.15l-.017 -.617l-.004 -.318v-.648l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.455 -4.687 2.333 -6.65 6.906 -7.166l.29 -.03l.553 -.046l.727 -.041l.15 -.006l.617 -.017c.21 -.003 .424 -.005 .642 -.005zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" />
+          </svg>
+        </span>
+        <span><bold>Russian and Ukranian translations are currently being reworked.</bold></span>
       </div>
     </div>
   );
